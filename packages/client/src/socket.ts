cd2a40ee9e13942e960f367be7a0020c6e6cfacd
veniic <veniaminguidea.r@gmail.com>
2026-09-13 20:45:57 +0300

    Change server URL to production endpoint

--- packages/client/src/socket.ts
+++ packages/client/src/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client'
 import type { ClientToServerEvents, ServerToClientEvents } from '@foaica/shared'
 
-const SERVER_URL = (import.meta.env.VITE_SERVER_URL as string | undefined) ?? 'http://localhost:3001'
+const SERVER_URL = (import.meta.env.VITE_SERVER_URL as string | undefined) ?? 'https://foasca.onrender.com'
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SERVER_URL, {
   autoConnect: true,
