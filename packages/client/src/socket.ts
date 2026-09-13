c07819f811d7ce625e5fca4586550936fd952a77
veniic <veniaminguidea.r@gmail.com>
2026-09-13 20:39:01 +0300

    Add files via upload

--- packages/client/src/socket.ts
+++ packages/client/src/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client'
 import type { ClientToServerEvents, ServerToClientEvents } from '@foaica/shared'
 
-const SERVER_URL = (import.meta.env.VITE_SERVER_URL as string | undefined) ?? 'https://foasca.onrender.com'
+const SERVER_URL = (import.meta.env.VITE_SERVER_URL as string | undefined) ?? 'http://localhost:3001'
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SERVER_URL, {
   autoConnect: true,
